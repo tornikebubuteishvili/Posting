7d1231b0c685516426b0526f10889eafee9c37fa
tornike bubuteishvili <tbubu14@freeuni.edu.ge>
2018-11-28 23:05:18 +0400

    test commit

---
+++ PostContentUpdatedDomainEventHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Posting.Application.Interfaces;
+using Posting.Domain.AggregatesModel.PostAggregate;
+using Posting.Domain.AggregatesModel.PosterAggregate;
+using Posting.Domain.Events.Posting;
+using Posting.Domain.Exceptions;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Posting.Application.DomainEventHandlers
+{
+    public class PostContentUpdatedDomainEventHandler : INotificationHandler<PostContentUpdatedDomainEvent>
+    {
+
+        private readonly IRepository<Poster> _posterRepository;
+
+        public PostContentUpdatedDomainEventHandler(IRepository<Poster> posterRepository)
+        {
+            _posterRepository = posterRepository ?? throw new ArgumentNullException(nameof(posterRepository));
+        }
+
+        public async Task Handle(PostContentUpdatedDomainEvent notification, CancellationToken cancellationToken)
+        {
+
+            var uploadedContents = notification.NewUploadedContents.Select(newUploadedContent =>
+                new UploadedContent(newUploadedContent.Content, newUploadedContent.UploadedContentTypeId,
+                    newUploadedContent.Capacity)).ToList();
+
+            var poster = await _posterRepository.GetByIdAsync(notification.PosterId);
+
+            if (poster == null)
+                throw new PosterNotFoundException($"Poster with id {notification.PosterId} not found");
+
+            poster.UpdatePostContent(uploadedContents);
+
+            _posterRepository.Update(poster);
+        }
+    }
+}
---
+++ UpdateCommentCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Posting.Application.Helpers;
+using Posting.Application.Interfaces;
+using Posting.Domain.AggregatesModel.CommentAggregate;
+using Posting.Domain.Exceptions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Posting.Application.Command
+{
+    public class UpdateCommentCommandHandler : IRequestHandler<UpdateCommentCommand, bool>
+    {
+
+        private readonly IRepository<Comment> _commentRepository;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public UpdateCommentCommandHandler(IRepository<Comment> commentRepository,
+            ILoggerFactory loggerFactory)
+        {
+            _commentRepository = commentRepository ?? throw new ArgumentNullException(nameof(commentRepository));
+            _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+        }
+
+        public async Task<bool> Handle(UpdateCommentCommand request, CancellationToken cancellationToken)
+        {
+            var uploadedContents = UploadedContentHelper.Extract(request);
+
+            var comment = await _commentRepository.GetByIdAsync(request.CommentId);
+
+            if (comment == null)
+                throw new CommentNotFoundException($"Comment with id {request.CommentId} was not found");
+
+            if (!comment.HasOwner(request.PosterId))
+                throw new PosterIsNotCommentOwnerException(
+                    $"Poster with id {request.PosterId} is not allowed to delete comment with id {request.CommentId}");
+
+            comment.UpdateContent(uploadedContents);
+
+            _commentRepository.Update(comment);
+
+            var result = await _commentRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            _loggerFactory.CreateLogger(nameof(UpdateCommentCommandHandler))
+                .LogTrace(
+                    $"Poster with id {request.PosterId} updated comment with id {request.CommentId} successfully");
+
+            return result;
+        }
+    }
+}
